08a94a496820e6a9f150334272cff7c4e573c843
Brando <brandon@rjohnsononline.com>
2014-10-28 13:34:57 -0500

    Added index.php as a page type called "Front Page".

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
+<?php
+/*
+Template Name: Front Page
+*/
+?>
+
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
     <?php the_content(); ?>
+    <?php the_field('middle_content_area') ?>
+    <?php the_field('bottom_content_area') ?>
 <?php endwhile; endif; ?>
+
+
 
 <?php get_footer(); ?>
