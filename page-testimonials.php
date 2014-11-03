814c93d3f70bde22fad7e5c16869dd901683950b
Brando <brandon@rjohnsononline.com>
2014-11-03 08:53:59 -0600

    Attempting to remove top margin from woocommerce pages.

--- page-testimonials.php
+++ page-testimonials.php
@@ -5,10 +5,6 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
-
-    <ul class="row123">
-        <li class="col-md-12"><?php the_content(); ?></li>
-    </ul>
+       <?php the_content(); ?>
 <?php endwhile; endif; ?>
-
 <?php get_footer(); ?>
