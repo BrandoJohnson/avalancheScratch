a842fb1b41c384de796f5dc976034e2b52763efc
Brando <brandon@rjohnsononline.com>
2014-11-03 09:00:28 -0600

    Attempting to remove top margin from woocommerce pages.

--- page-testimonials.php
+++ page-testimonials.php
@@ -5,7 +5,7 @@
 <?php get_header(); ?>
 
 <?php
-    $page_id = 16; // substitute page_id of page you want content included from for "2"
+    $page_id = 2; // substitute page_id of page you want content included from for "2"
     $page = get_post($page_id);
     echo "<p>$page->post_content</p>";
 ?>
