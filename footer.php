2214d2d58464c36fd9a4e9b23d7a0c3d63c27e1e
Brando <brandon@rjohnsononline.com>
2014-11-09 13:29:07 -0600

    test

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
                                 <nav class="foot" align="center">
                                     <a href="index-2.html">HOME</a> | <a href="eshop/10browse.html">PRODUCTS</a> | <a href="dams.html">ICE DAMS</a> | <a href="testimonials.html">TESTIMONIALS</a> | <a href="contact.html">CONTACT US</a> | <a href="sitemap.html">SITE MAP</a> | <a href="dealer.html">FIND A DEALER</a> | <a href="gallery.html">GALLERY</a> | <a href="galleryfilelibrary.html">LIBRARY</a>
                                     <br />
-                                   &copy; <?php echo date('Y'); ?> Avalanche! Products by Rhino Manufacturing - 800-378-3088 -  <a href="<?php echo home_url(); ?>/policies">Store Policies</a> -  <a href="mailto:sales@avalanche-snow.com">E-mail</a>  <!--<a href="/downloads/Avalanche-order-form.pdf" target="_blank">Fax Order Form</a>-->
+                                   &copy; <?php echo date('Y'); ?> Avalanche! Products by Rhino Manufacturing - 800-378-3088 a -  <a href="<?php echo home_url(); ?>/policies">Store Policies</a> -  <a href="mailto:sales@avalanche-snow.com">E-mail</a>  <!--<a href="/downloads/Avalanche-order-form.pdf" target="_blank">Fax Order Form</a>-->
                                 </nav>
                               </td>
                         <td valign="top" background="<?php print IMAGES; ?>/border_right.gif"><img src="<?php print IMAGES; ?>/spacer.gif" width="20" height="40" border="0" alt="" /><br /><br /></td>
