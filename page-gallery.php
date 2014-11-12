a1c075ddd5cc9ffd850981c17d3d63847f9d3609
Brando <brandon@rjohnsononline.com>
2014-11-11 23:24:11 -0600

    center youtube div

--- page-gallery.php
+++ page-gallery.php
@@ -6,7 +6,7 @@
 <?php get_header(); ?>
 
 <?php if ( function_exists('show_nivo_slider') ) { show_nivo_slider(); } ?>
-<br /><br /><br />
+<br /><br /><br /><br />
 <div class="youtube"><iframe width="560" height="315" src="//www.youtube.com/embed/LIW7QzsJMYM" frameborder="0" allowfullscreen></iframe></div>
 
 <?php get_footer(); ?>
