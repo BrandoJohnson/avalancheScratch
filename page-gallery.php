54845192359bebe6da19cd6ada91d19d5ef5aa1f
Brando <brandon@rjohnsononline.com>
2014-11-11 23:17:13 -0600

    youtube iframe added to gallery page

--- page-gallery.php
+++ page-gallery.php
@@ -7,5 +7,6 @@
 
 <?php if ( function_exists('show_nivo_slider') ) { show_nivo_slider(); } ?>
 <br /><br /><br />
+<iframe width="560" height="315" src="//www.youtube.com/embed/LIW7QzsJMYM" frameborder="0" allowfullscreen></iframe>
 
 <?php get_footer(); ?>
