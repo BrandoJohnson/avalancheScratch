9e6aa87f5098887a0e59ea0882980933e35be921
Brando <brandon@rjohnsononline.com>
2014-11-15 17:02:00 -0600

    spacing

--- header-shop.php
+++ header-shop.php
@@ -91,15 +91,3 @@
     <table cellspacing="0" cellpadding="0" border="0" width="742" bgcolor="#ffffff" valign="top">
         <tr>
             <td valign="top"><img src="<?php print IMAGES; ?>/spacer.gif" width="714" height="12" border="0" alt="" /><br />
-                <script>
-                    jQuery(document).ready(function($){
-                        $('#slides').slides({
-                            preload: true,
-                            preloadImage: '<?php echo get_template_directory_uri(); ?>/images/loading.gif',
-                            play: 5000,
-                            pause: 2500,
-                            hoverPause: true
-
-                        });
-                    });
-                </script>
