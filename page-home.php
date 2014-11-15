bc0ea7d38b93c302e0e6fb0e226f628a11d6d738
Brando <brandon@rjohnsononline.com>
2014-11-15 17:21:32 -0600

    spacing

--- page-home.php
+++ page-home.php
@@ -6,11 +6,11 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
-    <ul class="row123">
-        <li class="col-md-12 row1234"><?php the_content(); ?></li>
-        <li class="col-md-12"><?php the_field('middle_content_area') ?></li>
-        <li class="col-md-12"><?php the_field('bottom_content_area') ?></li>
-    </ul>
+    <div class="row123">
+        <div class="col-md-12 row1234"><?php the_content(); ?></div>
+        <div class="col-md-12"><?php the_field('middle_content_area') ?></div>
+        <div class="col-md-12"><?php the_field('bottom_content_area') ?></div>
+    </div>
 
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
