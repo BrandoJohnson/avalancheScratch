d9195737333950264c06eef54304cbcd4fe6dab1
Brando <brandon@rjohnsononline.com>
2014-11-15 14:28:30 -0600

    om

--- page-home.php
+++ page-home.php
@@ -8,7 +8,7 @@
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
 
     <ul class="row123" style="border: 1px solid red;">
-        <li class="col-md-12 row1234"><?php the_content(); ?></li>
+        <li class="col-md-12 row1234">xxxx<?php the_content(); ?></li>
         <li class="col-md-12"><?php the_field('middle_content_area') ?></li>
         <li class="col-md-12"><?php the_field('bottom_content_area') ?></li>
     </ul>
