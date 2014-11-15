84115d927c66b91024067526a338c1a30aa96f0b
Brando <brandon@rjohnsononline.com>
2014-11-15 16:38:46 -0600

    spacing

--- footer.php
+++ footer.php
@@ -3,9 +3,6 @@
 
                 </table>
 
-                    <map name="headmap">
-                        <area alt="Email" coords="160,37,388,55" href="mailto:sales@avalanche-snow.com">
-                    </map>
             </div>
         <?php wp_footer(); ?>
     </body>
