e6b4c4935185aed87eb01b8a2c70f173e51a7b58
Brando <brandon@rjohnsononline.com>
2014-11-15 14:45:02 -0600

    Test

--- page-home.php
+++ page-home.php
@@ -11,5 +11,6 @@
         <li class="col-md-12"><?php the_field('middle_content_area') ?></li>
         <li class="col-md-12"><?php the_field('bottom_content_area') ?></li>
     </ul>
+
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
