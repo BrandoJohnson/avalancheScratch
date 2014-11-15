2498588796e624e7e62cca995790b8b67bfd652c
Brando <brandon@rjohnsononline.com>
2014-11-15 16:37:02 -0600

    spacing

--- footer.php
+++ footer.php
@@ -1,21 +1,6 @@
                         <td valign="top" >
                         </td>
-                    <tr>
-                        <td valign="top" background="<?php print IMAGES; ?>/border_left.gif"><img src="<?php print IMAGES; ?>/spacer.gif" width="10" height="40" border="0" alt="" /><br /></td>
-                            <td bgcolor="#eeeeee">
-                                <nav class="foot botnav" align="center">
-                                    <a href="index-2.html">HOME</a> | <a href="eshop/10browse.html">PRODUCTS</a> | <a href="dams.html">ICE DAMS</a> | <a href="testimonials.html">TESTIMONIALS</a> | <a href="contact.html">CONTACT US</a> | <a href="sitemap.html">SITE MAP</a> | <a href="dealer.html">FIND A DEALER</a> | <a href="gallery.html">GALLERY</a> | <a href="galleryfilelibrary.html">LIBRARY</a>
-                                    <br />
-                                   &copy; <?php echo date('Y'); ?> Avalanche! Products by Rhino Manufacturing - 800-378-3088 -  <a href="<?php echo home_url(); ?>/policies">Store Policies</a> -  <a href="mailto:sales@avalanche-snow.com">E-mail</a>  <!--<a href="/downloads/Avalanche-order-form.pdf" target="_blank">Fax Order Form</a>-->
-                                </nav>
-                              </td>
-                        <td valign="top" ><img src="<?php print IMAGES; ?>/spacer.gif" width="0" height="40" border="0" alt="" /><br /><br /></td>
-                    </tr>
-                    <tr>
-                        <td valign="top" colspan="3">
-                            <img src="<?php print IMAGES; ?>/border_bottom.gif" width="754" height="20" border="0" alt="" /><br />
-                        </td>
-                    </tr>
+
                 </table>
 
                     <map name="headmap">
--- page-home.php
+++ page-home.php
@@ -5,5 +5,12 @@
 
 <?php get_header(); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post();?>
+    <ul class="row123">
+        <li class="col-md-12 row1234"><?php the_content(); ?></li>
+        <li class="col-md-12"><?php the_field('middle_content_area') ?></li>
+        <li class="col-md-12"><?php the_field('bottom_content_area') ?></li>
+    </ul>
 
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
