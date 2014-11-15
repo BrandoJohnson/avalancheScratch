767cd45bb365f9f006bde272db14dc05c0bcbdf3
Brando <brandon@rjohnsononline.com>
2014-11-15 17:05:39 -0600

    spacing

--- header-shop.php
+++ header-shop.php
@@ -88,3 +88,6 @@
         </nav>
         <div class="orgBar"></div>
     </div>
+    <table cellspacing="0" cellpadding="0" border="1" width="742" bgcolor="#ffffff" valign="top">
+        <tr>
+            <td valign="top"><img src="<?php print IMAGES; ?>/spacer.gif" width="714" height="12" border="0" alt="" /><br style="clear: both;" />
