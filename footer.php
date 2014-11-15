73bc21f86e61e2cf3625c4b884b7bad9a5576215
Brando <brandon@rjohnsononline.com>
2014-11-15 16:53:23 -0600

    spacing

--- footer.php
+++ footer.php
@@ -1,14 +1,5 @@
 
                         <tr>
-                            <td bgcolor="#eeeeee">
-                                <nav class="foot botnav" align="center">
-                                    <a href="index-2.html">HOME</a> | <a href="eshop/10browse.html">PRODUCTS</a> | <a href="dams.html">ICE DAMS</a> | <a href="testimonials.html">TESTIMONIALS</a> | <a href="contact.html">CONTACT US</a> | <a href="sitemap.html">SITE MAP</a> | <a href="dealer.html">FIND A DEALER</a> | <a href="gallery.html">GALLERY</a> | <a href="galleryfilelibrary.html">LIBRARY</a>
-                                    <br />
-                                    &copy; <?php echo date('Y'); ?> Avalanche! Products by Rhino Manufacturing - 800-378-3088 -  <a href="<?php echo home_url(); ?>/policies">Store Policies</a> -  <a href="mailto:sales@avalanche-snow.com">E-mail</a>  <!--<a href="/downloads/Avalanche-order-form.pdf" target="_blank">Fax Order Form</a>-->
-                                </nav>
-                            </td>
-                            </tr>
-                        <tr>
                             <td valign="top">
                                 <img src="<?php print IMAGES; ?>/border_bottom.gif" width="754" height="20" border="0" alt="" /><br />
                             </td>
