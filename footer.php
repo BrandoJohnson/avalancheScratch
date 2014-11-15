dc699760a876691222126ffd31be10ef10dce5a6
Brando <brandon@rjohnsononline.com>
2014-11-15 16:54:27 -0600

    spacing

--- footer.php
+++ footer.php
@@ -8,7 +8,6 @@
                                 </nav>
                             </td>
                             </tr>
-
                 </table>
                     <map name="headmap">
                         <area alt="Email" coords="160,37,388,55" href="mailto:sales@avalanche-snow.com">
