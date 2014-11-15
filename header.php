f0e44a9582aa7868cef47766f9bc412dde989891
Brando <brandon@rjohnsononline.com>
2014-11-15 16:20:40 -0600

    spacing

--- header.php
+++ header.php
@@ -90,7 +90,7 @@
     </div>
     <table cellspacing="0" cellpadding="0" border="0" width="754" bgcolor="#ffffff" valign="top">
         <tr>
-            <td valign="top" background="<?php print IMAGES; ?>/border_left.gif" style="width: 5px;"></td>
+            <td valign="top" background="<?php print IMAGES; ?>/border_left.gif" style="width: 10px;"></td>
             <td valign="top"><img src="<?php print IMAGES; ?>/spacer.gif" width="714" height="12" border="0" alt="" /><br />
 
 
