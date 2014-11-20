d09553be68b2bd6c5be5c244b05105eda3012cb2
Brando <brandon@rjohnsononline.com>
2014-11-20 16:00:03 -0600

    Made contact div boxes taller to fit text.

--- page-contact.php
+++ page-contact.php
@@ -7,7 +7,7 @@
 
 <div style="width:351px;float:left;"> <img src="<?php print IMAGES; ?>/contact_head.gif" width="350" height="70" border="0" alt=""><br>
     <div style="width:350px;background-color:#F1F1F1;margin:10px 0px 0px 0px;">
-        <div class="body" style="padding:7px 10px 0px 30px; height:225px;">
+        <div class="body" style="padding:7px 10px 0px 30px; height: 350px;">
             <div style="width:143px;float:right;margin:40px 0px 0px 0px;"> <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d179799.044110832!2d-93.99306325332033!3d45.24051125337265!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b49621f8305403%3A0x3cd04582a2b81610!2s400+Congress+St+W%2C+Maple+Lake%2C+MN+55358!5e0!3m2!1sen!2sus!4v1405707276258" width="143" height="126" frameborder="0" style="border:0"></iframe><br>
                 <div align="center"><A HREF="https://goo.gl/maps/oNGyG" target="_blank">Click to see a larger map.</a><br>
                 </div>
@@ -32,7 +32,7 @@
 <div style="width:351px;float:right;">
     <div style="height:70px; background:#5998c9;"><a href="downloads/Avalanche_dealer.pdf" target="_blank"><img src="<?php print IMAGES; ?>/contact_dealerbutton.gif" width="167" height="70" border="0" alt="Dealer Information"></a></div>
     <div style="width:351px; background-color:#F1F1F1;margin:10px 0px 0px 0px;">
-        <div class="body" style="margin:0px 10px 0px 30px; height:225px;"><br>
+        <div class="body" style="margin:0px 10px 0px 30px; height:350px;"><br>
             <p><span style="COLOR: #000000" class="bigger"><strong>European&nbsp;Distributor -&nbsp;</strong></span>Avalanche! in Europe - by Yello Industries AS</p>
             <div><span class="tiny">Storgata 18B | Gjovik, Norway -&nbsp;</span><strong style="font-size: 10px;">Phone:</strong><span style="font-size: 10px;"> +47 815 22 333</span></div>
             <p><span class="tiny"><strong>Email:</strong>&nbsp;<span style="text-decoration: underline;">post@</span></span><span class="tiny"><a href="mailto:sales@avalanche-snow.eu">avalanche-snow.eu</a>&nbsp;</span><span class="tiny"><strong>Website:</strong> </span><span class="tiny"><a href="http://www.avalanche-snow.eu/">www.avalanche-snow.eu/</a></span></p>
