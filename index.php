5185ef180be60c5737992cf07ece2edef3dc6eb1
Brando <brandon@rjohnsononline.com>
2014-11-26 13:27:23 -0600

    Added 2.5em margin to index.php Default Template page type.

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 
-
 <?php get_header(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post();?>
-    <?php the_content(); ?>
-<?php endwhile; endif; ?>
-
-
+<div style="margin: 2.5em;">
+    <?php if (have_posts()) : while (have_posts()) : the_post();?>
+        <?php the_content(); ?>
+    <?php endwhile; endif; ?>
+</div>
 
 <?php get_footer(); ?>
