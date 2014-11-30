733193d9c5edad6602023676f97e78cef4e74839
Brando <brandon@rjohnsononline.com>
2014-11-30 17:22:09 -0600

    style edits

--- page-home.php
+++ page-home.php
@@ -9,7 +9,7 @@
     <div class="row123">
         <div class="col-md-12"><?php the_content(); ?></div>
         <br clear="all" >
-
+        <div class="col-md-12"><?php the_field('middle_content_area') ?></div>
         <div class="col-md-12"><?php the_field('bottom_content_area') ?></div>
     </div>
 
