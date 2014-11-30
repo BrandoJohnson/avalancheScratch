8832578118586a720bad0db5b6897e47862599ab
Brando <brandon@rjohnsononline.com>
2014-11-30 17:08:02 -0600

    Remove old style calls

--- page-home.php
+++ page-home.php
@@ -8,8 +8,8 @@
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
     <div class="row123">
         <div class="col-md-12 row1234"><?php the_content(); ?></div>
-        <br style="clear: both;">
-        <div class="col-md-12"><?php the_field('middle_content_area') ?></div>
+        <br style="clear: both;" >
+        <div><?php the_field('middle_content_area') ?></div>
         <div class="col-md-12"><?php the_field('bottom_content_area') ?></div>
     </div>
 
