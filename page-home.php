9c32707cb23adfc21f38442d29ad02641f611a12
Brando <brandon@rjohnsononline.com>
2014-11-30 17:18:55 -0600

    style edits

--- page-home.php
+++ page-home.php
@@ -7,7 +7,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
     <div class="row123">
-        <div class="col-md-12"><?php the_content(); ?></div>
+
         <br clear="all" >
         <div class="col-md-12"><?php the_field('middle_content_area') ?></div>
         <div class="col-md-12"><?php the_field('bottom_content_area') ?></div>
