256538a6288f0bf27377603d7d939bb81f6c83cb
Brando <brandon@rjohnsononline.com>
2014-11-30 20:03:19 -0600

    Style edits

--- header-shop.php
+++ header-shop.php
@@ -56,6 +56,6 @@
         </nav>
         <div class="orgBar"></div>
     </div>
-    <div class="frontpage-content" style="padding-left: 12px !important;">
+    <div class="frontpage-content" style="padding-left: 12px !important; ">
 
 
