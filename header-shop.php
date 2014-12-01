87fac880450ab721e1103d11b285524ecee00ac9
Brando <brandon@rjohnsononline.com>
2014-11-30 18:33:39 -0600

    dd

--- header-shop.php
+++ header-shop.php
@@ -16,7 +16,7 @@
 </head>
 
 <body  <?php body_class(); ?>bgcolor="#BCC0C7" background="<?php print IMAGES; ?>/bg.gif">
-<div align="center">
+<div align="center" style="margin: 0 auto;">
     <table cellspacing="0" cellpadding="0" border="0" width="783" valign="top">
         <tr>
             <td valign="top"><img src="<?php print IMAGES; ?>/head01.jpg" width="783" height="46" border="0" alt="" /><br /></td>
@@ -67,5 +67,4 @@
         <div class="orgBar"></div>
     </div>
     <div class="frontpage-content">
-        xxxxxxxx
 
