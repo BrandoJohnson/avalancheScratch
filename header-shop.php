61d0d92b315ff711af689a038cd0a7728c1924e3
Brando <brandon@rjohnsononline.com>
2014-11-30 20:28:21 -0600

    Style edits

--- header-shop.php
+++ header-shop.php
@@ -16,7 +16,7 @@
 </head>
 
 <body  <?php body_class(); ?>bgcolor="#BCC0C7" background="<?php print IMAGES; ?>/bg.gif">
-<div style="margin-left: 20% !important; margin-right: auto !important; width: 782px;" >
+<div style="margin-left: 40% !important; margin-right: auto !important; width: 782px;" >
     <table cellspacing="0" cellpadding="0" border="0" width="783" valign="top">
         <tr>
             <td valign="top"><img src="<?php print IMAGES; ?>/head01.jpg" width="783" height="46" border="0" alt="" /><br /></td>
