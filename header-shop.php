be6dd545b54f8fec2a2716b3eb8e81b2d3194a04
Brando <brandon@rjohnsononline.com>
2014-11-30 20:01:39 -0600

    Style edits

--- header-shop.php
+++ header-shop.php
@@ -56,6 +56,6 @@
         </nav>
         <div class="orgBar"></div>
     </div>
-    <div class="frontpage-content" style="margin-left: 12px;">
+    <div class="frontpage-content" style="margin-left: 12px; ">
 
 
