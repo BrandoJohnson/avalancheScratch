ecf1dd2b5a1ebc676fbc625c0f50ab7c992ec427
Brando <brandon@rjohnsononline.com>
2014-11-30 19:47:01 -0600

    Style edits

--- footer.php
+++ footer.php
@@ -16,6 +16,10 @@
                         <area alt="Email" coords="160,37,388,55" href="mailto:sales@avalanche-snow.com">
                     </map>
             </div>
+
+</div>
+</div>
+
         <?php wp_footer(); ?>
     </body>
 </html>
