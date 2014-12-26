c75ca11edb6dec9be931a78c6da5b16118796cbc
Brando <brandon@rjohnsononline.com>
2014-12-26 12:57:45 -0600

    Added blog section to homepage and enabled widgets.

--- functions.php
+++ functions.php
@@ -38,5 +38,26 @@ if (function_exists('add_theme_support')) {
 }
 
 
+
+
+/**********************************************************************/
+/* Register our sidebars and widgetized areas. */
+/**********************************************************************/
+
+function arphabet_widgets_init() {
+
+    register_sidebar( array(
+        'name' => 'sidebar-1',
+        'id' => 'sidebar-1',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="rounded">',
+        'after_title' => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+
+
+
 ?>
 
--- page-home.php
+++ page-home.php
@@ -2,7 +2,6 @@
     /* Template Name: Homepage */
 ?>
 
-
 <?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
@@ -12,6 +11,10 @@
         <div class="col-md-12"><?php the_field('middle_content_area') ?></div>
         <div class="col-md-12"><?php the_field('bottom_content_area') ?></div>
     </div>
-
+    <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+        <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+            <?php dynamic_sidebar( 'sidebar-1' ); ?>
+        </div><!-- #primary-sidebar -->
+    <?php endif; ?>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
