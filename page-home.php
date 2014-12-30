ff38a3ab370b77ef6c801e89499ec94afe3316bf
Brando <brandon@rjohnsononline.com>
2014-12-30 13:05:48 -0600

    Margin for blog images

--- page-home.php
+++ page-home.php
@@ -14,7 +14,7 @@
     <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
         <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
             <hr/>
-
+ccc
             <?php dynamic_sidebar( 'sidebar-1' ); ?>
         </div><!-- #primary-sidebar -->
     <?php endif; ?>
